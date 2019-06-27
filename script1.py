5527103d6b638e790a242e76c2c47c12ad977457
DragosSim <dragos_simionescu@yahoo.com>
2019-06-27 21:31:45 +0200

    message

--- script1.py
+++ script1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
-from matplotlib import
+from matplotlib import pyplot as plt
 
 s1 = np.random.random(10)
 s2 = pd.date_range(start = dt.datetime(2018, 1, 1), freq = 'm', periods = 10)
@@ -9,6 +9,6 @@ print(s1)
 print(s2)
 
 df = pd.DataFrame(s1, s2, columns=['abc'])
-print(df)
-
+plt.bar( df.index, df['abc'])
+plt.show()
 
